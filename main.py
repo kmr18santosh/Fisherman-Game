39d869ffcc5ad7d18c91a238599d2df504716628
Santosh Kumar <kmr18.santosh@gmail.com>
2022-08-24 17:26:18 +0530

    third commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-#new comment added
+import random
 pygame.init()
 
 screen=pygame.display.set_mode((800,600))
@@ -8,7 +8,6 @@ icon=pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
 #adding player
-
 playerImg=pygame.image.load('sailing-boat.png')
 playerX=400
 playerY=480
@@ -17,6 +16,14 @@ playerY_change=0
 pressed=0
 def player(X,Y):
     screen.blit(playerImg,(X,Y))
+
+#adding shark
+sharkImg=pygame.image.load('shark.png')
+sharkX=random.randint(0,736)
+sharkY=random.randint(0,500)
+sharkx_change=0.3
+def shark(X,Y):
+    screen.blit(sharkImg,(X,Y))
 
 
 
@@ -47,7 +54,7 @@ while running:
 
     playerX+=playerX_change
     playerY+=playerY_change
-
+    #Bounding the Boat within frame
     if playerX<0:
         playerX=0
     if playerY<0:
@@ -55,8 +62,20 @@ while running:
     if playerX>736:
         playerX=736
     if playerY>536:
-        playerY=536
+        playerY=536 
+    #Bounding the shraks within Frame
+    if sharkX<0:
+        sharkx_change=0.3
+    if sharkX>736:
+        sharkx_change=-0.3
+
+    #Moving the shark
+    sharkX+=sharkx_change
+
+
+    shark(sharkX,sharkY)
     player(playerX,playerY)
+
     pygame.display.update()
 
     
