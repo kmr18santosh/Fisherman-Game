cbedb9b1b0ca0b0ca4ad22899f63bf01a00acd9f
Santosh Kumar <kmr18.santosh@gmail.com>
2022-08-25 03:17:01 +0530

    fuel indicator added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from gc import DEBUG_SAVEALL
 import pygame
 import random
 import math
@@ -15,11 +16,8 @@ playerY=500
 playerX_change=0
 playerY_change=0
 pressed=0
-def player(X,Y):
-    screen.blit(playerImg,(X,Y))
            
 #adding sharks
-
 sharkImg=[]
 sharkX=[]
 sharkY=[]
@@ -40,33 +38,73 @@ for i in range(num_of_sharks-1):
 sharkImg.append(pygame.image.load('kraken.png'))
 sharkX.append(random.randint(0,736))
 sharkY.append(random.randint(0,400))
-sharkX_change.append(0.3)
+sharkX_change.append(0.6)
 sharkY_change.append(40)
 shark_frame_check.append(0)
 
+#adding fish
+
+
+def player(X,Y):
+    screen.blit(playerImg,(X,Y))
 
 def shark(X,Y,i):
     screen.blit(sharkImg[i],(X,Y))
 
-
 def isCollision(sharkX, sharkY, boatX, boatY):
-    distance = math.sqrt(math.pow(sharkX - boatX, 2) + (math.pow(sharkY - boatY, 2)))
-    if distance < 27:
+    gap = math.sqrt(math.pow(sharkX - boatX, 2) + (math.pow(sharkY - boatY, 2)))
+    if gap < 27:
         return True
     else:
         return False
 
+def disatnce(oldX, oldY, newX, newY):
+    distance = math.sqrt(math.pow(oldX - newX, 2) + (math.pow(oldY - newY, 2)))
+    return distance
+
+boat_health = 20
+boat_fuel=30
+fish_count=0
+font = pygame.font.Font('freesansbold.ttf', 18)
+def show_boat_health():
+    score = font.render("Boat Health : " + str(boat_health), True, (255, 255, 255))
+    screen.blit(score, (10,10))
+
+
+def game_over_text():
+    over_font = pygame.font.Font('freesansbold.ttf', 64)
+    over_text = over_font.render("GAME OVER!", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+
+def show_catch_fish():
+    catch_text = font.render("Fish Caught: " + str(fish_count),True,(255,255,255))
+    screen.blit(catch_text,(10,38))
+
+def show_fuel():
+    if boat_fuel<0:
+        fuel_text=font.render("Boat Fuel:EMPTY",True,(255,255,255))
+    else:
+        fuel_text=font.render("Boat Fuel:" + str(boat_fuel),True,(255,255,255))
+    screen.blit(fuel_text,(600,10))
+
+#Boat Running Parameteres
+startX=playerX
+startY=playerY
+endX=0
+endY=0
 
 
 running = True
 while running:
-    screen.fill((0,0,128))
+    screen.fill((6,11,84))
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
 
         if event.type==pygame.KEYDOWN:
             pressed+=1
+            startX=playerX
+            startY=playerY
             if event.key==pygame.K_LEFT:
                 playerX_change=-0.3                
             if event.key==pygame.K_RIGHT:
@@ -84,7 +122,14 @@ while running:
             or event.key==pygame.K_UP or event.key==pygame.K_DOWN):
                 playerX_change=0
                 playerY_change=0
-            
+            endX=playerX
+            endY=playerY
+
+            travel=disatnce(startX, startY, endX, endY)
+            boat_fuel -= travel/50
+
+
+
     #Bounding the Boat within frame
     if playerX<0:
         playerX=0
@@ -97,6 +142,13 @@ while running:
     
     
     for i in range(num_of_sharks):
+
+         # Game Over
+        if boat_health < 0 or boat_fuel<0:
+            for j in range(num_of_sharks):
+                sharkY[j] = 2000
+            game_over_text()
+            break
         
         #Moving the shark
         sharkX[i]+=sharkX_change[i]
@@ -124,13 +176,27 @@ while running:
         elif sharkY[i]<0:
             shark_frame_check[i]=0
 
+        #checking collision
+        collision = isCollision(sharkX[i], sharkY[i], playerX, playerY)
+        if collision:
+            #explosionSound = mixer.Sound("explosion.wav")
+            #explosionSound.play()
+            if i==num_of_sharks-1:
+                boat_health -= 5
+            else:
+                boat_health -= 1
+            sharkX[i] = random.randint(0, 736)
+            sharkY[i] = random.randint(0, 736)
+
     
 
     playerX+=playerX_change
     playerY+=playerY_change
     
     player(playerX,playerY)
-
+    show_boat_health()
+    show_catch_fish()
+    show_fuel()
 
     pygame.display.update()
 
