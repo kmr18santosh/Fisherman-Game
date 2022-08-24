dbe509f9889a298422814ef49b2080be1eaba5c1
Santosh Kumar <kmr18.santosh@gmail.com>
2022-08-25 03:52:31 +0530

    goldfish added

--- main.py
+++ main.py
@@ -43,6 +43,21 @@ sharkY_change.append(40)
 shark_frame_check.append(0)
 
 #adding fish
+fishImg=[]
+fishX=[]
+fishY=[]
+fishX_change=[]
+fishY_change=[]
+fish_frame_check=[]
+num_of_fishes=5
+
+for i in range(num_of_fishes):
+    fishImg.append(pygame.image.load('fishR.png'))
+    fishX.append(random.randint(0,736))
+    fishY.append(random.randint(0,400))
+    fishX_change.append(0.1)
+    fishY_change.append(20)
+    fish_frame_check.append(0)
 
 
 def player(X,Y):
@@ -51,9 +66,25 @@ def player(X,Y):
 def shark(X,Y,i):
     screen.blit(sharkImg[i],(X,Y))
 
-def isCollision(sharkX, sharkY, boatX, boatY):
+direction=0
+def fish(X,Y,i):
+    if direction%2==0:
+        fishImg[i]=(pygame.image.load('fishR.png'))
+    else:
+        fishImg[i]=(pygame.image.load('fishL.png'))
+    screen.blit(fishImg[i],(X,Y))
+
+
+def shark_Collision(sharkX, sharkY, boatX, boatY):
     gap = math.sqrt(math.pow(sharkX - boatX, 2) + (math.pow(sharkY - boatY, 2)))
     if gap < 27:
+        return True
+    else:
+        return False
+
+def fish_Collision(sharkX, sharkY, boatX, boatY):
+    gap = math.sqrt(math.pow(sharkX - boatX, 2) + (math.pow(sharkY - boatY, 2)))
+    if gap < 35:
         return True
     else:
         return False
@@ -62,10 +93,13 @@ def disatnce(oldX, oldY, newX, newY):
     distance = math.sqrt(math.pow(oldX - newX, 2) + (math.pow(oldY - newY, 2)))
     return distance
 
+#PLAYER PARAMETERS
 boat_health = 20
 boat_fuel=30
 fish_count=0
 font = pygame.font.Font('freesansbold.ttf', 18)
+
+
 def show_boat_health():
     score = font.render("Boat Health : " + str(boat_health), True, (255, 255, 255))
     screen.blit(score, (10,10))
@@ -140,7 +174,7 @@ while running:
     if playerY>536:
         playerY=536 
     
-    
+    #MANAGING SHARKS
     for i in range(num_of_sharks):
 
          # Game Over
@@ -177,7 +211,7 @@ while running:
             shark_frame_check[i]=0
 
         #checking collision
-        collision = isCollision(sharkX[i], sharkY[i], playerX, playerY)
+        collision = shark_Collision(sharkX[i], sharkY[i], playerX, playerY)
         if collision:
             #explosionSound = mixer.Sound("explosion.wav")
             #explosionSound.play()
@@ -187,6 +221,49 @@ while running:
                 boat_health -= 1
             sharkX[i] = random.randint(0, 736)
             sharkY[i] = random.randint(0, 736)
+            
+    #MANAGING FISHES
+    for i in range(num_of_fishes):
+
+        #Moving the fishes
+        fishX[i]+=fishX_change[i]
+        
+        #Bounding the fishes within Frame
+        if fish_frame_check[i]==0:
+            if fishX[i]<0:
+                fishX_change[i]=0.1
+                fishY[i]+=fishY_change[i]
+                direction+=1
+            if fishX[i]>770:
+                fishX_change[i]=-0.1
+                fishY[i]+=fishY_change[i]
+                direction+=1
+        elif fish_frame_check[i]==1: 
+            if fishX[i]<0:
+                fishX_change[i]=0.1
+                fishY[i]-=fishY_change[i]
+                direction+=1
+            if fishX[i]>770:
+                fishX_change[i]=-0.1
+                fishY[i]-=fishY_change[i] 
+                direction+=1
+
+        #blitting the fishes
+        fish(fishX[i],fishY[i],i)
+        if fishY[i]>500:
+            shark_frame_check[i]=1
+        elif fishY[i]<0:
+            shark_frame_check[i]=0
+
+
+        #checking collision
+        collision = fish_Collision(fishX[i], fishY[i], playerX, playerY)
+        if collision:
+            #explosionSound = mixer.Sound("explosion.wav")
+            #explosionSound.play()
+            fish_count += 1
+            fishX[i] = random.randint(0, 736)
+            fishY[i] = random.randint(0, 736)
 
     
 
